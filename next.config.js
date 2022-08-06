3f10964ccdeb2d2347914ad5af441a1329d29fb4
Ali gh <ghasempour.2m@gmail.com>
2022-08-06 15:58:59 +0430

    url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   env: {
-    BASE_URL: 'http://localhost:3000/api'
+    BASE_URL: 'https://e-commerce-project-alighdeveloper.vercel.app/'
   }
 }
 
