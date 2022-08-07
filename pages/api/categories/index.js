c3c116d57731a504c3df91259ef485ca57a28434
Ali gh <ghasempour.2m@gmail.com>
2022-08-07 13:19:41 +0430

    changin

--- pages/api/categories/index.js
+++ pages/api/categories/index.js
@@ -11,8 +11,6 @@ export default function handler(req, res){
             return getCats(req, res);
         case 'POST':
             return createCat(req, res);
-        case 'OPTIONS': 
-            return res.status(200).send('ok')
     }
 };
 
@@ -34,11 +32,6 @@ async function createCat(req, res) {
 
 async function getCats(req, res) {
     const cats = await Categories.find();
-    
-    res.setHeader('Access-Control-Allow-Origin', 'https://e-commerce-project-one.vercel.app')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
 
     res.status(200).json(cats)
 }
