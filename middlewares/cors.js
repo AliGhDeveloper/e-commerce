1341cc2021f45d36165a1b468919fdaab7232e8d
Ali gh <ghasempour.2m@gmail.com>
2022-08-07 14:44:49 +0430

    changin

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,9 +1,7 @@
 export default function preflight(req,res) {
-    if( req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization'); // If needed
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        res.status(200).json({})
-    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.status(200).json({})
 }
