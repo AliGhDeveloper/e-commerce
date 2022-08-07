3033e162b13c71e0ea9d329a369c55eb978c0669
Ali gh <ghasempour.2m@gmail.com>
2022-08-07 13:15:33 +0430

    changing

--- pages/api/categories/index.js
+++ pages/api/categories/index.js
@@ -36,5 +36,9 @@ async function getCats(req, res) {
     const cats = await Categories.find();
     
     res.setHeader('Access-Control-Allow-Origin', 'https://e-commerce-project-one.vercel.app')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     res.status(200).json(cats)
 }
